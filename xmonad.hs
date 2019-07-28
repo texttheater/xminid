758b5582851b423fbca16a3bbd70383f682d3b8d
Kilian Evang <noreply@texttheater.net>
2019-07-29 01:12:01 +0200

    Fix fullscreen

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,7 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName (setWMName)
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 
@@ -14,12 +16,40 @@ main = xmonad $ gnomeConfig {
     ],
     startupHook = composeAll [
         startupHook gnomeConfig,
+        -- fix fullscreen for Firefox
+        -- https://gist.github.com/sboehler/5f48017a6b53805485180a9a6d81196b
+        setFullscreenSupported,
         -- start update-manager on startup because we don't have the normal
         -- Ubuntu session to do that for us
         spawn "update-manager"
+    ],
+    handleEventHook = composeAll [
+        handleEventHook gnomeConfig,
+        -- fix fullscreen
+        fullscreenEventHook
     ]
 } `additionalKeys` [
     -- Workaround for
     -- https://bugs.launchpad.net/ubuntu/+source/xmonad/+bug/1813049
     ((mod4Mask .|. shiftMask, xK_q), spawn "gnome-session-quit --logout")
     ]
+
+setFullscreenSupported :: X ()
+setFullscreenSupported = withDisplay $ \dpy -> do
+    r <- asks theRoot
+    a <- getAtom "_NET_SUPPORTED"
+    c <- getAtom "ATOM"
+    supp <- mapM getAtom [
+        "_NET_WM_STATE_HIDDEN",
+        "_NET_WM_STATE_FULLSCREEN", -- XXX Copy-pasted to add this line
+        "_NET_NUMBER_OF_DESKTOPS",
+        "_NET_CLIENT_LIST",
+        "_NET_CLIENT_LIST_STACKING",
+        "_NET_CURRENT_DESKTOP",
+        "_NET_DESKTOP_NAMES",
+        "_NET_ACTIVE_WINDOW",
+        "_NET_WM_DESKTOP",
+        "_NET_WM_STRUT"
+        ]
+    io $ changeProperty32 dpy r a c propModeReplace (fmap fromIntegral supp)
+    setWMName "xmonad"
